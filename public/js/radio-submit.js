eb79d4555a0deaafbe9888f260cc4442610dd2be
average-alien <allentang113@gmail.com>
2022-09-17 17:30:12 -0700

    changes some buttons

--- public/js/radio-submit.js
+++ public/js/radio-submit.js
@@ -1,8 +1,8 @@
 // enables submission of form when clicking on radio input (without submit button)
 const radio = document.querySelectorAll('.btn-check')
-// console.log(radio)
+
 radio.forEach(button =>{
     button.addEventListener('change', (e) => {
-        e.path[1].requestSubmit()
+        e.path[2].requestSubmit()
     })
 })
